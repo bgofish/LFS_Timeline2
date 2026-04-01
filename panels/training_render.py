85ffaf5840cb7ed1f019aaa8929be87bf59bf783
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-01 19:58:54 +1300

    Update training_render.py

--- panels/training_render.py
+++ panels/training_render.py
@@ -1,9 +1,13 @@
+def setup_data_binding(context):
+    # This will be used to sync your spreadsheet data
+    pass
+
 class KeyframeSpreadsheetPanel:
     id = "TIMELINE_PT_spreadsheet"
     label = "Keyframe Spreadsheet"
-    # This file must exist in your plugin folder
+    # Ensure training_render.rml is in the same folder as this file
     panel_path = "training_render.rml" 
 
     def draw(self, context):
-        # UI is handled by your RML file
+        # UI logic is handled by the RML file
         pass
